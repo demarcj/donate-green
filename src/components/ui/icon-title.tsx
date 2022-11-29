b18f4d4f5c771227094d54d686e6f49bd0a77575
demarcj <demarcjohnson@gmail.com>
2022-11-28 23:28:17 -0500

    Added active link styling and nav img to the nav

--- src/components/ui/icon-title.tsx
+++ src/components/ui/icon-title.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from 'styles/icon-title.module.css';
-import img from 'earth_icon.png';
+import img from 'images/earth.png';
 
 export const IconTitle: React.FC = () => {
   return (
     <div className={styles.icon_title_container}>
-      <img className={styles.img} src={img}/>
+      <img className={styles.img} src={img} alt="Earth"/>
       <h1 className={styles.title}>Donate Green</h1>
     </div>
   )
--- src/components/ui/nav-bar.tsx
+++ src/components/ui/nav-bar.tsx
@@ -1,19 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import nav from "styles/nav.module.css";
+import growth from "images/growth.png";
+import home from "images/home.png";
+import person from "images/person.png";
+import growth_active from "images/growth_active.png";
+import home_active from "images/home_active.png";
+import person_active from "images/person_active.png";
 
 export const NavBar: React.FC = () => {
+  const [path, set_path] = useState(`home`);
+
+  const get_path = (active: string) => {
+    set_path(active);
+  }
+
   return (
     <nav className={nav.nav}>
       <ul className={nav.nav_list}>
         <li className={nav.nav_item}>
-          <Link to={`/home`} className={nav.nav_text}>Home</Link>
+          <NavLink 
+            to={`/home`} 
+            className={({ isActive }) =>{
+                if(isActive){
+                  get_path(`home`)
+                }
+                return isActive ? [nav.nav_text, nav.active].join(" ") : nav.nav_text
+              }
+            }
+          >
+            <img src={path.includes(`home`) ? home_active : home} alt="" />
+          </NavLink>
         </li>
         <li className={nav.nav_item}>
-          <Link to={`/activities`} className={nav.nav_text}>Activities</Link>
+          <NavLink 
+            to={`/activities`} 
+            className={({ isActive }) =>{
+                if(isActive){
+                  get_path(`activities`)
+                }
+                return isActive ? [nav.nav_text, nav.active].join(" ") : nav.nav_text
+              }
+            }
+          >
+            <img src={path.includes(`activities`) ? growth_active : growth} alt="" />
+          </NavLink>
         </li>
         <li className={nav.nav_item}>
-          <Link to={`/profile`} className={nav.nav_text}>Profile</Link>
+          <NavLink 
+            to={`/profile`} 
+            className={({ isActive }) =>{
+                if(isActive){
+                  get_path(`profile`)
+                }
+                return isActive ? [nav.nav_text, nav.active].join(" ") : nav.nav_text
+              }
+            }
+          >
+            <img src={path.includes(`profile`) ? person_active : person} alt="" />
+          </NavLink>
         </li>
       </ul>
     </nav>
