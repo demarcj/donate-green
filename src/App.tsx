f1fcc7bb94a79a1dfcac06f6c35c329a3b4348bc
demarcj <demarcjohnson@gmail.com>
2022-12-08 23:57:10 -0500

    Added a catch all redirect and got rid of the portal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Portal } from './components/layout';
+import { Start, Signup, Home, Profile, Activities } from "components/layout";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { NavBar } from "components/ui";
+import start from 'styles/start.module.css';
 import './App.css';
 
 const App: React.FC = () => {
+  const is_startpage = window.location.pathname.length <= 1;
+
   return (
-    <Portal />
+    <main className={start.main}>
+      <div className={is_startpage ? start.start_container : ``}>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={ <Navigate to="/" /> } />
+        </Routes>
+      </div>
+      <div className={is_startpage ? start.no_show : ``}>
+        <NavBar />
+      </div>
+    </main>
   );
-}
+};
 
 export default App
--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,6 +1,5 @@
 export * from './activities';
 export * from './home';
-export * from './portal';
 export * from './profile';
 export * from './signup';
 export * from './start';
