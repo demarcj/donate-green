4f16a358855f7c99e886bf7d60e2ca046fbade8f
DeMarc Johnson <demarcjohnson@gmail.com>
2023-01-28 23:33:58 -0500

    Made some design changes to the payment page

--- src/components/layout/payment.tsx
+++ src/components/layout/payment.tsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { NavBar, Header, Divider, NavBack, DonateInput } from "components/ui";
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
 import styles from "styles/payment.module.css";
 import button from "styles/button.module.css";
 
 export const Payment: React.FC = () => {
   const [donate, set_donate] = useState(0);
 
+  const navigate = useNavigate();
   const search = useLocation().search;
   const get_id = new URLSearchParams(search).get(`id`);
   const id = get_id ? get_id : ``;
   const get_parameter_index  = window.location.pathname.lastIndexOf(`payment`);
   const pathname = window.location.pathname.substring(0, (get_parameter_index - 1));
   const url = `${pathname}?id=${id}`
-
-  const get_donate_amount = (num: number) => {
-    set_donate(num)
-  };
+  const get_donate_amount = (num: number) => set_donate(num);
+  const handle_donate = () => {
+    navigate(`home`);
+  }
 
   return (
     <div className={styles.main_container}>
@@ -43,8 +45,8 @@ export const Payment: React.FC = () => {
           <div>Total</div>
           <div>${donate}</div>
         </div>
+        <div className="test"></div>
         <Divider />
-        {/* <h2></h2> */}
         <FormControl>
           <FormLabel id="demo-radio-buttons-group-label">Payment Options</FormLabel>
             <RadioGroup
@@ -52,17 +54,60 @@ export const Payment: React.FC = () => {
               defaultValue="card"
               name="radio-buttons-group"
             >
-            <FormControlLabel value="card" control={<Radio />} label="Card" />
-            <FormControlLabel value="paypal" control={<Radio />} label="PayPal" />
-            <FormControlLabel value="other" control={<Radio />} label="Other" />
+            <FormControlLabel 
+              value="card" 
+              label="Card" 
+              control={
+                <Radio 
+                  sx={{
+                    mr: "auto"
+                  }}
+                />
+              } 
+            />
+            <FormControlLabel 
+              value="paypal" 
+              label="PayPal" 
+              control={
+                <Radio 
+                  sx={{
+                    mr: "auto"
+                  }}
+                />
+              } 
+            />
+            <FormControlLabel 
+              value="other" 
+              label="Other" 
+              control={
+                <Radio 
+                  sx={{
+                    mr: "auto"
+                  }}
+                />
+              } 
+            />
           </RadioGroup>
         </FormControl>
         <Divider />
-        <div>
-          <div>Confirmation of payment</div>
-        </div>
+        <FormControlLabel 
+            label="Confirmation of payment" 
+            control={
+              <Checkbox 
+                sx={{
+                  mr: "auto"
+                }}
+              />
+            } 
+        />
         <div className={styles.button}>
-          <button className={button.default}>Donate</button>
+          <button 
+            className={button.default} 
+            onClick={handle_donate}
+            disabled
+          >
+            Donate
+          </button>
         </div>
       </div>
       <NavBar />
